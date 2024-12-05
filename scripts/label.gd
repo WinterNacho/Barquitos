70393a228b88964e728ef474cff34f93e9f79aa5
José Pablo Arriagada Quinteros <arriagadajoseq@gmail.com>
2024-12-05 00:48:18 -0300

    hud listo con estado, falta tipo de bala

--- scripts/label.gd
+++ scripts/label.gd
@@ -1,19 +1,46 @@
 extends Label
 
-@onready var vida: int
+@onready var playerName: String
 @onready var player: CharacterBody3D
+@onready var vida: int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
-func set_player(node):
+
+
+func set_player(node,player_data):
 	player = node
-	vida = player.current_health
-	self.text = str(vida)
+	
+	playerName = player_data.name
+	print(playerName)
+	self.text = playerName
+	vida = player.max_health
+	$Vida.text = str(vida)
+	
+func update_icon():
+	var  current = player.actual_state
+	match current:
+		0:
+			$State.texture = null
+		1:
+			$State.texture = preload("res://scenes/ui/HUD/snail.png")
+		2:
+			$State.texture = preload("res://scenes/ui/HUD/snowflake.png")
+		3:
+			$State.texture = preload("res://scenes/ui/HUD/confused.png")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if player==null:
+		self.visible = false
+	else:
+		update_icon()
+		pass
+		
 	if vida:
 		vida = player.current_health
-		self.text = str(vida)
+		$Vida.text = str(vida)
 	pass
+	
+	
